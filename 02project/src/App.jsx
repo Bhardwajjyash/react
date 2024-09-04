3fd15bdf41cc5159a5b473f677b9a1cfe6b852da
bhardwajjyash <demousehack@gmail.com>
2024-09-05 00:43:14 +0530

    commit

---
+++ 02project/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let[counter,setCounter] = useState(15)
+  //let counter =15
+  const addvalue = () => {
+    //counter = counter +1
+    setCounter(counter+1)
+  }
+  const removevalue = () => {
+    setCounter(counter-1)
+  }
+  
+  
+
+  return (
+    <>
+      <h1>YASH BHARDWAJ REACT</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addvalue}>Add value{counter}</button>
+      <br />
+      <button onClick={removevalue}>Remove value {counter}</button>
+      <p>footer : {counter}</p>
+    </>
+  )
+}
+
+export default App
